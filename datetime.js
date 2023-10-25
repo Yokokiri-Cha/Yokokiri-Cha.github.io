669ca8c228a333801f7f4ce4e2ef8f8bb27db729
横茶横葉 <115348123+Yokokiri-Cha@users.noreply.github.com>
2023-10-25 11:30:01 +0900

    Update datetime.js

--- datetime.js
+++ datetime.js
@@ -1,14 +1,3 @@
-function showClock2() {
-   var fulldata = new Date();
-   var year = fulldata.getFullYear();
-  var month=fulldata.getMonth()+1;
-  var day=fulldata.getDate();
-  var dayvalue=fulldata.getDay();
-  var temp=new Array("日曜日","月曜日","火曜日","水曜日","木曜日","金曜日","土曜日");
-document.getElementById("datevalue").innerHTML=("今日は"+year+"年"+month+"月"+day+"日"+temp[dayvalue]+"です")
-}
-setInterval('showClock2()',1000);
-
 function set2fig(num) {
    var ret;
    if( num < 10 ) { ret = "0" + num; }
@@ -22,5 +11,12 @@ function showClock2() {
    var nowSec  = set2fig( nowTime.getSeconds() );
    var msg =  nowHour + ":" + nowMin + ":" + nowSec;
    document.getElementById("Clockvalue").innerHTML = msg;
+      var fulldata = new Date();
+   var year = fulldata.getFullYear();
+  var month=fulldata.getMonth()+1;
+  var day=fulldata.getDate();
+  var dayvalue=fulldata.getDay();
+  var temp=new Array("日曜日","月曜日","火曜日","水曜日","木曜日","金曜日","土曜日");
+document.getElementById("datevalue").innerHTML=("今日は"+year+"年"+month+"月"+day+"日"+temp[dayvalue]+"です")
 }
 setInterval('showClock2()',1000);

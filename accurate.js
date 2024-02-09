79fde169952ca2758f396b64f40b275962020d06
横茶横葉 (YokochaYokoha) <115348123+Yokokiri-Cha@users.noreply.github.com>
2024-02-09 14:57:27 +0900

    Create accurate.js

---
+++ accurate.js
@@ -0,0 +1,13 @@
+function sl(pos){
+
+
+	lat = pos.coords.latitude;
+	lon = pos.coords.longitude;
+
+
+	document.getElementById("table_lat").innerHTML = lat;
+	document.getElementById("table_lon").innerHTML = lon;
+}
+setInterval('sl(pos)',1000);
+//参考:https://systemexpress.co.jp/htmlcss/geolocation.html
+//ありがとうございます!!
